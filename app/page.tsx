74cf22decb747b7e690a477b47617931b511218c
katsuhiro-eqol <katsuhiro.yamashita.sr01@gmail.com>
2024-10-21 20:23:55 +0900

    20241021

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import Link from "next/link";
+'use client';
+import { useState, useEffect } from 'react';
+import { useLiff } from '@/components/LiffProvider';
 
-export default function Home() {
+
+export default function Login() {
+    const { liff } = useLiff();
+
+  useEffect(() => {
+    if (liff?.isLoggedIn()) {
+        console.log("loginしました")
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get('token');
+        console.log("token", token)
+    } else {
+        console.log("loginしてません")
+        liff?.login()
+    }
+  }, [liff]);
+
   return (
     <div>
-      <div>Home</div>
-      <Link href="/login">Loginページへ</Link>
-      </div>
-
+        {(liff?.isLoggedIn())  ? (
+            <div>ログインしました</div>
+        ):(
+            <button onClick={() => liff?.login()} className='bg-blue-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-blue-600'>
+            login
+            </button>
+        )}
+    </div>
   );
 }
